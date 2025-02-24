d3410ccf82a42bc0b51f4d1441c62f4fe705e1d3
Ayush201738982361 <ayushnair87@gmail.com>
2025-02-25 00:47:22 +0530

    Created server side APIs

---
+++ backend/routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { createNewUser, loginUser } = require("../controller/user");
+
+const router = express.Router();
+
+router.post("/signup", createNewUser);
+router.post("/login", loginUser);
+
+module.exports = router;
